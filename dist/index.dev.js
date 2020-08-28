bd39070f04b6c5658f6106751232ea4d686af824
developervilchez <developervilchez@gmail.com>
2020-08-28 12:06:08 -0500

    delete dist folder

--- dist/index.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var express = require('express');
-
-var app = express();
-var PORT = process.env.PORT || 4001;
-app.listen(PORT, function () {
-  console.log("listen ".concat(PORT));
-});
