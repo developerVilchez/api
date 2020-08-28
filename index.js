badea0478f4dfae3aceeabe4714d6d69dc480bb9
developervilchez <developervilchez@gmail.com>
2020-08-28 12:14:44 -0500

    implement competitions endpoint

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const fetch = require('node-fetch');
 const config = require('./config');
 const app = express();
 
+app.get('/competitions', (req, res) => {
+  fetch(`${config.api.host}${config.api.resources}?access_token=${config.api.token}`)
+    .then(res => res.json())
+    .then(json => res.send(json))
+    .catch(err => res.status(404).send(err))
+})
+
 const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => {
