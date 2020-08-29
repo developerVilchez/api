7a5899d7228d6f8413ec3a2e7fb89dbb2538f860
developervilchez <developervilchez@gmail.com>
2020-08-28 21:32:44 -0500

    add teams by id endpoint

--- index.js
+++ index.js
@@ -27,5 +27,15 @@ app.get('/teams', (req, res) => {
     .catch(err => res.send({ path : `${err.path}`, msg:'revisa la url de la data'}))
 })
 
+app.get('/teams/:id', (req, res) => {
+  getDataFromJson('./data/teams.json', 'utf-8')
+    .then(data => JSON.parse(data.toString()))
+    .then(json => json["teams"])
+    .then(arr => arr.find(obj => obj["id"] === parseInt(req.params.id)))
+    .then(data => data !== "undefined" ? res.send(data) : null)
+    .catch(err => console.error(err))
+    
+})
+
 module.exports = app;
 
