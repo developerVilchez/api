38f6979978297f939782ffd13fa8823b7be65b38
developervilchez <developervilchez@gmail.com>
2020-08-28 21:51:33 -0500

    add data players.json

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/teams', (req, res) => {
   getDataFromJson('./data/teams.json', 'utf-8')
     .then(data => res.send(JSON.parse(data.toString())))
     .catch(err => res.send({ path : `${err.path}`, msg:'revisa la url de la data'}))
-})
+});
 
 app.get('/teams/:id', (req, res) => {
   getDataFromJson('./data/teams.json', 'utf-8')
@@ -34,8 +34,9 @@ app.get('/teams/:id', (req, res) => {
     .then(arr => arr.find(obj => obj["id"] === parseInt(req.params.id)))
     .then(data => data !== "undefined" ? res.send(data) : null)
     .catch(err => console.error(err))
-    
-})
+  });
+
+  
 
 module.exports = app;
 
